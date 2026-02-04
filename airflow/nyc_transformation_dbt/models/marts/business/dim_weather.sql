41e88e86d80a3dbe31cc4110253653a2d8626183
Ahmadou0306 <ahmadou.ndiaye030602@gmail.com>
2026-02-04 20:03:10 +0000

    dim weather

--- airflow/nyc_transformation_dbt/models/marts/business/dim_weather.sql
+++ airflow/nyc_transformation_dbt/models/marts/business/dim_weather.sql
@@ -1,4 +1,18 @@
-WITH weather AS (
+WITH weather_impact AS (
     SELECT
-    FROM {{ref("stg_weather")}}
+        -- Dimension temporelle
+        weather_date,        
+        weather_temp_min_celcius,
+        weather_temp_max_celcius,
+        ROUND((weather_temp_min_celcius + weather_temp_max_celcius) / 2, 1) AS weather_temp_avg_celcius,
+        weather_condition,
+        weather_precip_mm,
+        weather_snow_mm,
+        is_extreme_snow,
+        is_extreme_rain,
+        is_extreme_temperature
+    FROM {{ref("int_weather_impact_daily")}}
 )
+
+SELECT *
+FROM weather_impact
